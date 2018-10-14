15b83c0273c28078c77db887b47f6103314163fa
Jonas Zeiger <jonas.zeiger@talpidae.net>
2018-10-14 15:01:47 +0200

    JaxRsBindingInspector: support directly bound resources

--- src/main/java/net/talpidae/base/util/injector/JaxRsBindingInspector.java
+++ src/main/java/net/talpidae/base/util/injector/JaxRsBindingInspector.java
@@ -5,6 +5,8 @@ import com.google.inject.Injector;
 
 import org.jboss.resteasy.util.GetRestful;
 
+import java.lang.reflect.Modifier;
+
 import javax.ws.rs.ext.Provider;
 
 import lombok.val;
@@ -14,6 +16,58 @@ import static net.talpidae.base.util.injector.BindingInspector.getTargetClass;
 
 public final class JaxRsBindingInspector
 {
+    private JaxRsBindingInspector()
+    {
+
+    }
+
+    /**
+     * Visit all JAX-RS Resource and Provider bindings.
+     */
+    public static void visitJaxRsBindings(Injector injector, ResourceBindingVisitor resourceVisitor, ProviderBindingVisitor providerVisitor)
+    {
+        for (; injector != null; injector = injector.getParent())
+        {
+            injector.getBindings().forEach((key, binding) ->
+            {
+                final Class<?> type = key.getTypeLiteral().getRawType();
+                if (type != null)
+                {
+                    val isTypeResource = GetRestful.isRootResource(type);
+                    if (isTypeResource)
+                    {
+                        val targetType = getTargetClass(binding);
+                        if (targetType != null)
+                        {
+                            // linked or provider binding, ie. bind(IBlub.class).to(BlubImpl.class)
+                            if (isConcrete(targetType))
+                            {
+                                resourceVisitor.accept(binding, type, targetType);
+                            }
+                        }
+                        else if (isConcrete(type))
+                        {
+                            // direct binding, eg. bind(BlablaImpl.class)
+                            resourceVisitor.accept(binding, type, type);
+                        }
+                    }
+
+                    if (type.isAnnotationPresent(Provider.class) && isConcrete(type))
+                    {
+                        providerVisitor.accept(binding, type);
+                    }
+                }
+            });
+        }
+    }
+
+
+    private static boolean isConcrete(Class<?> type)
+    {
+        return !type.isInterface() && !Modifier.isAbstract(type.getModifiers());
+    }
+
+
     @FunctionalInterface
     public interface ResourceBindingVisitor
     {
@@ -25,44 +79,5 @@ public final class JaxRsBindingInspector
     public interface ProviderBindingVisitor
     {
         void accept(Binding<?> binding, Class<?> type);
-    }
-
-
-    /**
-     * Visit all JAX-RS Resource and Provider bindings.
-     */
-    public static void visitJaxRsBindings(Injector injector, ResourceBindingVisitor resourceVisitor, ProviderBindingVisitor providerVisitor)
-    {
-        for (; injector != null; injector = injector.getParent())
-        {
-            for (val binding : injector.getBindings().values())
-            {
-                final Class<?> type = binding.getKey().getTypeLiteral().getRawType();
-                if (type != null)
-                {
-                    val isTypeResource = GetRestful.isRootResource(type);
-                    if (isTypeResource)
-                    {
-                        val targetType = getTargetClass(binding);
-                        if (targetType != null && !targetType.isInterface())
-                        {
-                            // register resources after providers
-                            resourceVisitor.accept(binding, type, targetType);
-                        }
-                    }
-
-                    if (type.isAnnotationPresent(Provider.class))
-                    {
-                        providerVisitor.accept(binding, type);
-                    }
-                }
-            }
-        }
-    }
-
-
-    private JaxRsBindingInspector()
-    {
-
     }
 }
