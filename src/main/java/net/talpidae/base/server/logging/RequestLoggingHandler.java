2eb398aba556b4422b302fc473d372110b8ec9ce
Jonas Zeiger <jonas.zeiger@talpidae.net>
2020-11-15 15:04:02 +0100

    RequestLoggingHandler: append user-agent and JWT sub field

--- src/main/java/net/talpidae/base/server/logging/RequestLoggingHandler.java
+++ src/main/java/net/talpidae/base/server/logging/RequestLoggingHandler.java
@@ -3,8 +3,12 @@ package net.talpidae.base.server.logging;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import lombok.extern.java.Log;
 import lombok.val;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 
 /**
@@ -13,6 +17,10 @@ import lombok.val;
 @Log
 public class RequestLoggingHandler implements HttpHandler
 {
+    private static final HttpString USER_AGENT = new HttpString("user-agent");
+
+    private static final HttpString AUTHORIZATION = new HttpString("authorization");
+
     private final HttpHandler next;
 
     public RequestLoggingHandler(final HttpHandler next)
@@ -54,10 +62,77 @@ public class RequestLoggingHandler implements HttpHandler
             sb.append(" tx=");
             sb.append(completedExchange.getResponseBytesSent());
 
+            val userAgent = completedExchange.getRequestHeaders().getFirst(USER_AGENT);
+            if (userAgent != null)
+            {
+                sb.append(" ua=");
+                sb.append(userAgent);
+            }
+
+            // extra a possible "sub" (subject) field from an "Authoriztation: Bearer" JWT token, if one exists
+            val authorization = completedExchange.getRequestHeaders().getFirst(USER_AGENT);
+            if (authorization != null)
+            {
+                // TODO: Also support logging Basic Auth username and other schemes/token formats
+                val subject = extractJwtSub(authorization);
+                if (subject != null)
+                {
+                    sb.append(" sub=");
+                    sb.append(subject);
+                }
+            }
+
             nextListener.proceed();
             log.info(sb.toString());
         });
 
         next.handleRequest(exchange);
+    }
+
+
+    private static String extractJwtSub(String authorization)
+    {
+        if (authorization != null)
+        {
+            int begin = authorization.indexOf("Bearer");
+            if (begin >= 0)
+            {
+                begin = authorization.indexOf('.', begin + 6);
+                if (begin > 0)
+                {
+                    int end = authorization.indexOf('.', begin + 1);
+                    if (end > begin)
+                    {
+                        try
+                        {
+                            String jwt = new String(Base64.getDecoder().decode(authorization.substring(begin + 1, end)), StandardCharsets.UTF_8);
+                            begin = jwt.indexOf("\"sub\"");
+                            if (begin > 0)
+                            {
+                                begin = jwt.indexOf(":", begin + 5);
+                                if (begin > 0)
+                                {
+                                    begin = jwt.indexOf('\"', begin + 1);
+                                    if (begin > 0)
+                                    {
+                                        end = jwt.indexOf('\"', begin + 1);
+                                        if (end > begin)
+                                        {
+                                            return jwt.substring(begin + 1, end);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch (RuntimeException e)
+                        {
+                            // ignore, just return null
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
